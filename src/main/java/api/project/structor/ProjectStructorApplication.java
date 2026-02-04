315ebba841033d99d31a036ec39eac9fd5cc5b3f
hunghx <baby10051999@gmail.com>
2026-02-04 19:41:09 +0700

    up

--- src/main/java/api/project/structor/ProjectStructorApplication.java
+++ src/main/java/api/project/structor/ProjectStructorApplication.java
@@ -1,13 +1,24 @@
 package api.project.structor;
 
+import api.project.structor.repositories.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectStructorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectStructorApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            System.out.println(studentRepository.countStudentByLengthName());
+            studentRepository.deleteByName("Van");
+        };
     }
 
 }
--- src/main/java/api/project/structor/repositories/StudentRepository.java
+++ src/main/java/api/project/structor/repositories/StudentRepository.java
@@ -1,12 +1,30 @@
 package api.project.structor.repositories;
 
 import api.project.structor.entity.bt.Student;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository // tao bean // ORM
 public interface StudentRepository extends JpaRepository<Student,Long> {
-//
+    // Truy vấn dữ liệu trong JPA
+    // Method Query : đặt tên -> tự tạo câu SQL
+
+    // Sử dụng @Query để định nghĩa Câu lệnh : JPQL và SQL
+    // Đếm số lượng sinh viên có tên >10 kí tự
+//    @Query(nativeQuery = true,value = "Select count(*) from students where char_length(student_name)>10")
+    @Query(value = "Select count(S) from Student S where length(S.name) > 10")
+    Long countStudentByLengthName();
+
+    @Query(value = "delete from Student where name ilike concat('%',:name,'%')")
+    @Transactional
+    @Modifying
+    void deleteByName(@Param("name") String name);
+
+    // Lưu ý : nếu truy vấn là DML : INSERT, UPDATE, DELETE thì phải thêm 2 chú thích : @Transactional, @Modify
 }
 
 
